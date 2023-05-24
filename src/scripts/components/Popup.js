5227ba1b36e9b5a97cb80adfa8be7c7fb6b19d78
Liubov Kushko <lyuba-0258@yandex.ru>
2023-05-24 12:31:34 +0700

    hotfix: убрать биндинг в удаляемом слушателе

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -2,18 +2,18 @@ export default class Popup {
   constructor({ popupSelector }) {
     this._popupSelector = popupSelector;
     this._popup = document.querySelector(popupSelector);
-    // this.open = this.open.bind(this);
+    this._handleEscClose = this._handleEscClose.bind(this)
   }
 
   open() {
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
     this._popup.classList.add('popup_opened');
     
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {
